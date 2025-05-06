4c916870d932565ce2b914967f69df55fbcf21bb
Noaaaa <ncox2004@gmail.com>
2025-05-06 10:21:30 +0100

    changed wave spawning and fixed navmesh holes

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WaveController : MonoBehaviour {
-
+ 
     public GameObject enemy;
     public int maxWaves = 2;
 
@@ -17,11 +18,13 @@ public class WaveController : MonoBehaviour {
     private bool roundEnded = false;
 
     private TMP_Text waveText;
+    private TMP_Text enemiesText;
 
     void Start() {
         goal = GameObject.Find("Goal").GetComponent<Goal>();
         enemiesContainer = GameObject.Find("Enemies");
-        waveText = GameObject.Find("WaveUI").GetComponent<TMP_Text>();
+        waveText = GameObject.Find("WaveNumberUI").GetComponent<TMP_Text>();
+        enemiesText = GameObject.Find("EnemiesRemainingUI").GetComponent<TMP_Text>();
     }
 
     void Update() {
@@ -40,7 +43,8 @@ public class WaveController : MonoBehaviour {
                 goal.disableGoal();
             }
 
-            waveText.text = $"Wave {waveNumber} / {maxWaves}";
+            enemiesText.text = $"{enemiesRemaining} enemies remaining";
+            waveText.text = $"Wave   {waveNumber} / {maxWaves}";
         }
     }
 
@@ -51,7 +55,17 @@ public class WaveController : MonoBehaviour {
             waveNumber++;
             currentMaxEnemyCount += 5;
             for (int i = 0; i < currentMaxEnemyCount; i++) {
-                Instantiate(enemy, new Vector3(Random.Range(-30.0f, 60.0f), 2.1f, Random.Range(10.0f, 30.0f)), Quaternion.identity, enemiesContainer.transform);
+
+                // generate a random point in the area
+                Vector3 randomPoint = new Vector3(Random.Range(-30.0f, 64.0f), -1.0f, Random.Range(-43.0f, 58.0f));
+
+                NavMeshHit hitResult;
+                // find the closest point on the NavMesh to the random point
+                if (NavMesh.SamplePosition(randomPoint, out hitResult, 1000.0f, NavMesh.AllAreas)) {
+                    // spawn an enemy in at that random position on the NavMesh
+                    Instantiate(enemy, new Vector3(hitResult.position.x, 2.1f, hitResult.position.z), Quaternion.identity, enemiesContainer.transform);
+                }
+        
             }
         }
 
